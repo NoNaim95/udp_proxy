a7b0964cab32708e050c909bd507cf35255c31c4
NoNaim95 <abraun006@gmail.com>
2022-10-04 00:29:33 +0200

    Minor Stuff

--- examples/logger.rs
+++ examples/logger.rs
@@ -1,67 +1,61 @@
-mod Inspector {
-    use std::{net::{SocketAddr, SocketAddrV4, UdpSocket}, env::set_current_dir};
-
+#[allow(dead_code)]
+mod inspector {
+    use std::net::SocketAddrV4;
     use udp_proxy::udp_proxy::Packet;
     pub struct Inspector {
         client: Option<SocketAddrV4>,
         dst: Option<SocketAddrV4>,
         sent_packets: Vec<Packet>,
-	number_of_intercepted_packets: u32
+        number_of_intercepted_packets: u32,
     }
     impl Inspector {
-        pub fn new(client: Option<SocketAddrV4>, dst: Option<SocketAddrV4>) -> Inspector{
-	    let i = Inspector{
-		client,
-		dst,
-		sent_packets: vec!(),
-		number_of_intercepted_packets: 0
-	    };
-	    i
-	}
+        pub fn new(client: Option<SocketAddrV4>, dst: Option<SocketAddrV4>) -> Inspector {
+            let i = Inspector {
+                client,
+                dst,
+                sent_packets: vec![],
+                number_of_intercepted_packets: 0,
+            };
+            i
+        }
 
         pub fn on_packet(&mut self, packet: &mut Packet) {
-	    self.number_of_intercepted_packets += 1;
-	    self.sent_packets.push(packet.clone());
-	    if self.number_of_intercepted_packets == 30{
-		for packet in &self.sent_packets{
-		    println!("{:02X?}", packet.data);
-		}
-	    }
+            self.number_of_intercepted_packets += 1;
+            self.sent_packets.push(packet.clone());
+            if self.number_of_intercepted_packets == 30 {
+                for packet in &self.sent_packets {
+                    println!("{:02X?}", packet.data);
+                }
+            }
         }
     }
 }
 
+use crate::inspector::Inspector;
 use udp_proxy::udp_proxy::*;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let mut proxy;
 
-
-    let src_ip: &str;
-    let src_port: &str;
-    let dst_ip: &str;
-    let dst_port: &str;
+    let (src_ip, src_port, dst_ip, dst_port): (&str, &str, &str, &str);
     if args.len() == 5 {
-        src_ip = &args[1];
-        src_port = &args[2];
-        dst_ip = &args[3];
-        dst_port = &args[4];
+        (src_ip, src_port, dst_ip, dst_port) = (&args[1], &args[2], &args[3], &args[4]);
         println!("Binding Proxy to {src_ip}:{src_port}");
         println!("Going to Forward packets to {dst_ip}:{dst_port}");
     } else {
-        src_ip = "127.0.0.1";
-        src_port = "4444";
-        dst_ip = "127.0.0.1";
-        dst_port = "28763";
+        (src_ip, src_port, dst_ip, dst_port) = ("127.0.0.1", "4444", "127.0.0.1", "28763");
         println!("No Command Line Args supplied, going to use the defaults");
         println!("Binding Proxy to 127.0.0.1:4444");
         println!("Going to Forward packets to 127.0.0.1:28763");
     }
 
-    let mut inspector = Inspector::Inspector::new(None, None);
-    let mut interceptor = |p: &mut Packet| {inspector.on_packet(p);};
+    let mut inspector = Inspector::new(None, None);
+    let mut interceptor = |p: &mut Packet| {
+        inspector.on_packet(p);
+    };
+
     proxy = ProxyServer::new(src_ip, src_port, dst_ip, dst_port, &mut interceptor);
-    println!("Starting to forward");
+    println!("Start forwarding");
     proxy.start_forwarding();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,96 +1,90 @@
 #[allow(dead_code)]
-mod utils;
-
-pub mod udp_proxy{
+pub mod udp_proxy {
     use std::net::{SocketAddr, SocketAddrV4, UdpSocket};
     use std::str;
 
     #[derive(Clone)]
-    pub struct Packet{
-	pub data: Vec<u8>,
-	sender: SocketAddrV4,
-	receiver: SocketAddrV4,
+    pub struct Packet {
+        pub data: Vec<u8>,
+        sender: SocketAddrV4,
+        receiver: SocketAddrV4,
     }
 
     pub struct ProxyServer<'a> {
-	socket: UdpSocket,
-	dest_addr: SocketAddrV4,
-	client: Option<SocketAddrV4>,
-	interceptor: &'a mut dyn FnMut(&mut Packet),
+        socket: UdpSocket,
+        dest_addr: SocketAddrV4,
+        client: Option<SocketAddrV4>,
+        interceptor: &'a mut dyn FnMut(&mut Packet),
     }
 
-    impl Packet{
-	fn new(buf: Vec<u8>, sender: SocketAddrV4, receiver: SocketAddrV4) -> Packet {
-	    let p = Packet {
-		data: buf,
-		sender,
-		receiver,
-	    };
-	    p
-	}
+    impl Packet {
+        fn new(buf: Vec<u8>, sender: SocketAddrV4, receiver: SocketAddrV4) -> Packet {
+            let p = Packet {
+                data: buf,
+                sender,
+                receiver,
+            };
+            p
+        }
     }
-
 
     impl<'a> ProxyServer<'a> {
-	pub fn new(
-	    local_ip: &str,
-	    local_port: &str,
-	    dst_ip: &str,
-	    dst_port: &str,
-	    f: &'a mut dyn FnMut(&mut Packet),
-	) -> ProxyServer<'a> {
-	    let proxy = ProxyServer {
-		socket: UdpSocket::bind(std::format!("{local_ip}:{local_port}"))
-		    .expect("couldn't bind to address"),
-		dest_addr: format!("{dst_ip}:{dst_port}")
-		    .parse()
-		    .expect("Given Destination Ip Address could not be parsed"),
-		client: None,
-		interceptor: f,
-	    };
-	    proxy
-	}
+        pub fn new(
+            local_ip: &str,
+            local_port: &str,
+            dst_ip: &str,
+            dst_port: &str,
 
-	pub fn start_forwarding(&mut self) {
-	    loop {
-		let mut buf = vec![0; 4096];
-		let (number_of_bytes_read, src_addr) = self
-		    .socket
-		    .recv_from(&mut buf)
-		    .expect("Did not receive any Data");
-		buf.resize(number_of_bytes_read, 0);
-		let sender = match src_addr {
-		    SocketAddr::V4(addr) => addr,
-		    SocketAddr::V6(_) => panic!("IpV6 is not supported yet"),
-		};
-		if sender != self.dest_addr {
-		    self.client = Some(sender);
-		}
-		let receiver = if sender == self.dest_addr {
-		    self.client.unwrap()
-		} else {
-		    self.dest_addr
-		};
-		let mut packet = Packet::new(buf, sender, receiver);
-		(self.interceptor)(&mut packet);
-		self.socket
-		    .send_to(&packet.data, packet.receiver)
-		    .expect("Could not send bytes to Destination");
-	    }
-	}
+            f: &'a mut dyn FnMut(&mut Packet),
+        ) -> ProxyServer<'a> {
+            let proxy = ProxyServer {
+                socket: UdpSocket::bind(std::format!("{local_ip}:{local_port}"))
+                    .expect("couldn't bind to address"),
+                dest_addr: format!("{dst_ip}:{dst_port}")
+                    .parse()
+                    .expect("Given Destination Ip Address could not be parsed"),
+                client: None,
+                interceptor: f,
+            };
+            proxy
+        }
 
-	pub fn get_dest(&self) -> SocketAddrV4{
-	    self.dest_addr
-	}
-	pub fn get_client(&self) -> Option<SocketAddrV4>{
-	    self.client
-	}
+        pub fn start_forwarding(&mut self) {
+            loop {
+                let mut buf = vec![0; 4096];
+                let (number_of_bytes_read, src_addr) = self
+                    .socket
+                    .recv_from(&mut buf)
+                    .expect("Did not receive any Data");
+                buf.resize(number_of_bytes_read, 0);
+                let sender = match src_addr {
+                    SocketAddr::V4(addr) => addr,
+                    SocketAddr::V6(_) => panic!("IpV6 is not supported yet"),
+                };
+                if sender != self.dest_addr {
+                    self.client = Some(sender);
+                }
+                let receiver = if sender == self.dest_addr {
+                    self.client.unwrap()
+                } else {
+                    self.dest_addr
+                };
+                let mut packet = Packet::new(buf, sender, receiver);
+                (self.interceptor)(&mut packet);
+                self.socket
+                    .send_to(&packet.data, packet.receiver)
+                    .expect("Could not send bytes to Destination");
+            }
+        }
+
+        pub fn get_dest(&self) -> SocketAddrV4 {
+            self.dest_addr
+        }
+        pub fn get_client(&self) -> Option<SocketAddrV4> {
+            self.client
+        }
     }
-
-
 }
-
-
 
 #[cfg(test)]
 mod tests {
